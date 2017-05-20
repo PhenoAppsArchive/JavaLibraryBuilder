83ea95ab35ecd6fe79abd95bf47370049f640e60
Peter Rosario <PeterRosario@ksu.edu>
2017-05-20 13:55:52 -0500

    Utils: Added simple test method. MainActivity: Added code to verify I am correctly using code in a foreign module.

--- app/src/main/java/org/wheatgenetics/javalibrarybuilder/MainActivity.java
+++ app/src/main/java/org/wheatgenetics/javalibrarybuilder/MainActivity.java
@@ -4,16 +4,25 @@ package org.wheatgenetics.javalibrarybuilder;
  * Uses:
  * android.os.Bundle
  * android.support.v7.app.AppCompatActivity
+ * android.widget.TextView
  *
+ * org.wheatgenetics.javalib.Utils
  * org.wheatgenetics.javalibrarybuilder.R
  */
 
 public class MainActivity extends android.support.v7.app.AppCompatActivity
 {
-    @Override
+    @java.lang.Override
     protected void onCreate(final android.os.Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         this.setContentView(org.wheatgenetics.javalibrarybuilder.R.layout.activity_main);
+
+        final int                     number   = 2;
+        final android.widget.TextView textView = (android.widget.TextView)
+            this.findViewById(org.wheatgenetics.javalibrarybuilder.R.id.textView);
+        assert textView != null;
+        textView.setText(java.lang.String.format("double(%d) is %d",
+            number, org.wheatgenetics.javalib.Utils.doubleOf(number)));
     }
 }
