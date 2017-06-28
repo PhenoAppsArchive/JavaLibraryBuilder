66cde8528f7a7f9c615b4c1d5d26add7385cdacd
Peter Rosario <PeterRosario@ksu.edu>
2017-06-28 16:24:13 -0500

    Utils: Made convert() take a length parameter in order to tell convert() which bytes to accept and which bytes to ignore.

--- javalib/src/main/java/org/wheatgenetics/javalib/Utils.java
+++ javalib/src/main/java/org/wheatgenetics/javalib/Utils.java
@@ -8,22 +8,25 @@ public class Utils extends java.lang.Object
     public static java.lang.String adjust(final java.lang.String unadjusted)
     { return org.wheatgenetics.javalib.Utils.makeEmptyIfNull(unadjusted).trim(); }
 
-    public static java.lang.String convert(final byte buffer[])
+    public static java.lang.String convert(final byte buffer[], int length)
     {
         if (null == buffer)
             return null;
         else
         {
+            length = java.lang.Math.max(0     , length       );          // Handle too small length.
+            length = java.lang.Math.min(length, buffer.length);          // Handle too big   length.
+
             java.lang.String s = "";
-
-            final int              last = buffer.length - 1;
-                  java.lang.String b                       ;
-            for (int i = 0; i <= last; i++)
             {
-                b = java.lang.Byte.toString(buffer[i]);
-                if (0 == i) s = b; else s += "," + b;
+                final int              last = length - 1;
+                      java.lang.String b                ;
+                for (int i = 0; i <= last; i++)
+                {
+                    b = java.lang.Byte.toString(buffer[i]);
+                    if (0 == i) s = b; else s += "," + b;
+                }
             }
-
             return s;
         }
     }
