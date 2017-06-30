286e0e8e9affa582a5f8faf2d7690e60f1f92c33
Peter Rosario <PeterRosario@ksu.edu>
2017-06-30 16:46:44 -0500

    Utils: Added relaceIfNull().

--- javalib/src/main/java/org/wheatgenetics/javalib/Utils.java
+++ javalib/src/main/java/org/wheatgenetics/javalib/Utils.java
@@ -2,8 +2,11 @@ package org.wheatgenetics.javalib;
 
 public class Utils extends java.lang.Object
 {
+    public static java.lang.String replaceIfNull(final java.lang.String s,
+    final java.lang.String valueIfNull) { return null == s ? valueIfNull : s; }
+
     public static java.lang.String makeEmptyIfNull(final java.lang.String s)
-    { return null == s ? "" : s; }
+    { return org.wheatgenetics.javalib.Utils.replaceIfNull(s, ""); }
 
     public static java.lang.String adjust(final java.lang.String unadjusted)
     { return org.wheatgenetics.javalib.Utils.makeEmptyIfNull(unadjusted).trim(); }
