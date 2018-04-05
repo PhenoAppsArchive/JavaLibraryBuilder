d1a523eac4c691b10a72238fa7d941b96ecf670f
Peter Rosario <PeterRosario@ksu.edu>
2018-04-05 09:13:38 -0500

    CsvWriter: Added another constructor.

--- javalib/src/main/java/org/wheatgenetics/javalib/CsvWriter.java
+++ javalib/src/main/java/org/wheatgenetics/javalib/CsvWriter.java
@@ -6,18 +6,20 @@ package org.wheatgenetics.javalib;
  */
 public class CsvWriter extends com.csvreader.CsvWriter
 {
+    // region Constructors
+    public CsvWriter(final java.io.Writer writer)
+    { super(/* outputStream => */ writer, /* delimiter => */ ','); }
+
     public CsvWriter(final java.io.File file) throws java.io.IOException
-    {
-        super(
-            /* outputStream => */ new java.io.FileWriter(file, false), // throws java.io.IOException
-            /* delimiter    => */ ','                                );
-    }
+    { this(/* writer => */ new java.io.FileWriter(file, false) /* throws java.io.IOException */ ); }
+    // endregion
 
     // region Public Methods
-    public void write() throws java.io.IOException { this.write(""); }
+    public void write() throws java.io.IOException
+    { this.write(""); /* throws java.io.IOException */ }
 
     public void write(final int content) throws java.io.IOException
-    { this.write(java.lang.String.valueOf(content)); }
+    { this.write(java.lang.String.valueOf(content)); /* throws java.io.IOException */ }
 
     public void write(final java.lang.String format, final java.lang.Object... args)
     throws java.io.IOException
