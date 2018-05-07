18a59b06f1fa41cc766248b91aa18907faf4b619
Peter Rosario <PeterRosario@ksu.edu>
2018-05-07 12:10:26 -0500

    Added package com.csvreader. Added CsvReader and CsvWriter.

---
+++ javalib/src/main/java/org/wheatgenetics/javalib/Dir.java
@@ -0,0 +1,81 @@
+package org.wheatgenetics.javalib;
+
+@java.lang.SuppressWarnings({"ClassExplicitlyExtendsObject"})
+public class Dir extends java.lang.Object
+{
+    // region Fields
+    private final java.io.File     path               ;
+    private final java.lang.String blankHiddenFileName;
+
+    private boolean exists;
+    // endregion
+
+    @java.lang.SuppressWarnings({"SimplifiableConditionalExpression"})
+    private static boolean createNewDir(final java.io.File parent, final java.lang.String child)
+    {
+        if (null == parent)
+            return false;
+        else
+        {
+            final java.io.File dir = null == child ? parent : new java.io.File(parent, child);
+            return dir.exists() ? false : dir.mkdirs();
+        }
+    }
+
+    protected  java.io.File getPath() { return this.path; }
+
+    // region Constructors
+    public Dir(final java.io.File parent, final java.lang.String child,
+    final java.lang.String blankHiddenFileName)
+    {
+        super();
+
+        this.path                = new java.io.File(parent, child);
+        this.blankHiddenFileName = blankHiddenFileName            ;
+
+        this.exists = this.path.exists();
+    }
+
+    public Dir(final org.wheatgenetics.javalib.Dir parent, final java.lang.String child)
+    { this(new java.io.File(parent.path.getPath()), child, parent.blankHiddenFileName); }
+    // endregion
+
+    // region Public Methods
+    public boolean getExists() { return this.exists; }
+
+    public java.io.File createIfMissing() throws java.io.IOException
+    {
+        if (!this.exists)
+        {
+            org.wheatgenetics.javalib.Dir.createNewDir(this.path, null);
+            assert null != this.path; this.exists = this.path.exists();
+        }
+
+        if (this.exists)
+        {
+            final java.io.File blankHiddenFile =
+                new java.io.File(this.path, this.blankHiddenFileName);
+            if (blankHiddenFile.createNewFile())
+                return blankHiddenFile;
+            else
+                throw new java.io.IOException("Couldn't create " + blankHiddenFile.getName());
+        }
+        else throw new java.io.IOException(this.path.getPath() + " does not exist");
+    }
+
+    @java.lang.SuppressWarnings({"ResultOfMethodCallIgnored"})
+    public java.io.File createNewFile(final java.lang.String fileName) throws java.io.IOException
+    {
+        if (this.exists)
+        {
+            final java.io.File file = new java.io.File(this.path, fileName);
+            file.createNewFile();                                      // throws java.io.IOException
+            return file;
+        }
+        else throw new java.io.IOException(this.path.getPath() + " does not exist");
+    }
+
+    public void createNewDir(final java.lang.String dirName)
+    { org.wheatgenetics.javalib.Dir.createNewDir(this.path, dirName); }
+    // endregion
+}
