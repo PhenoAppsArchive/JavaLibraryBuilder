c04b106ec2ff2b52f3d261794c6592b779274bdc
Peter Rosario <PeterRosario@ksu.edu>
2019-01-11 14:20:20 -0600

    Items: Added append().

--- javalib/src/main/java/org/wheatgenetics/javalib/mstrdtl/Items.java
+++ javalib/src/main/java/org/wheatgenetics/javalib/mstrdtl/Items.java
@@ -8,6 +8,7 @@ package org.wheatgenetics.javalib.mstrdtl;
 public interface Items
 {
     public void add(final org.wheatgenetics.javalib.mstrdtl.Item item);
+    public void append()                                              ;
 
     public int                                    size()                  ;
     public org.wheatgenetics.javalib.mstrdtl.Item get (final int position);
