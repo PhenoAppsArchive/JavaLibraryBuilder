8e5349b432b798cc8b21960d65023cea11cbfe25
Peter Rosario <PeterRosario@ksu.edu>
2019-08-05 14:30:29 -0500

    Removed unnecessary suppression.

--- javalib/src/main/java/org/wheatgenetics/javalib/mstrdtl/Items.java
+++ javalib/src/main/java/org/wheatgenetics/javalib/mstrdtl/Items.java
@@ -4,15 +4,13 @@ package org.wheatgenetics.javalib.mstrdtl;
  * Uses:
  * org.wheatgenetics.javalib.mstrdtl.Item
  */
-@java.lang.SuppressWarnings({"UnnecessaryInterfaceModifier", "unused"}) public interface Items
+@java.lang.SuppressWarnings({"UnnecessaryInterfaceModifier"}) public interface Items
 {
     @java.lang.SuppressWarnings({"unused"})
     public void add(final org.wheatgenetics.javalib.mstrdtl.Item item);
 
     @java.lang.SuppressWarnings({"unused"}) public void append();
-
-
-    @java.lang.SuppressWarnings({"unused"}) public int size();
+    @java.lang.SuppressWarnings({"unused"}) public int  size  ();
 
     @java.lang.SuppressWarnings({"unused"})
     public org.wheatgenetics.javalib.mstrdtl.Item get(final int position);
