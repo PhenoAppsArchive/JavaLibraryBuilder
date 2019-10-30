6b0aea94309d80bc41d5b6dbb561b60320a7ef00
Peter Rosario <PeterRosario@ksu.edu>
2019-10-30 13:56:24 -0500

    Added app code.

---
+++ app/src/main/java/org/wheatgenetics/javalibrarybuilder/WebViewActivity.java
@@ -0,0 +1,31 @@
+package org.wheatgenetics.javalibrarybuilder;
+
+/**
+ * Uses:
+ * android.content.Intent
+ * android.os.Bundle
+ * android.webkit.WebView
+ * androidx.appcompat.app.AppCompatActivity
+ *
+ * org.wheatgenetics.javalibrarybuilder.R
+ */
+public class WebViewActivity extends androidx.appcompat.app.AppCompatActivity
+{
+    static final java.lang.String CONTENT = "content", ENCODING = "encoding";
+
+    @java.lang.Override protected void onCreate(final android.os.Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        this.setContentView(org.wheatgenetics.javalibrarybuilder.R.layout.activity_web_view);
+
+        final android.content.Intent intent  = this.getIntent();
+        final android.webkit.WebView webView =
+            this.findViewById(org.wheatgenetics.javalibrarybuilder.R.id.webView);
+        if (null != webView) webView.loadData(
+            /* data => */ intent.getStringExtra(
+                org.wheatgenetics.javalibrarybuilder.WebViewActivity.CONTENT),
+            /* mimeType => */"text/html",
+            /* encoding => */ intent.getStringExtra(
+                org.wheatgenetics.javalibrarybuilder.WebViewActivity.ENCODING));
+    }
+}
